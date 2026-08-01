5699a119b9ac98ac1a12363111489f93317a16ce
sabrinahan93-byte <sabrinahan93@gmail.com>
2026-08-01 16:47:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from pcip.excel.workbook import DashboardWorkbook
 
+
+def main():
+
+    dashboard = DashboardWorkbook()
+
+    dashboard.initialize()
+
+
+if __name__ == "__main__":
+
+    main()
