72303ad6676231758e880f83b3dced10aecedf31
sabrinahan93-byte <sabrinahan93@gmail.com>
2026-08-01 17:04:43 +0800

    Update company_loader.py

--- pcip/company_loader.py
+++ pcip/company_loader.py
@@ -1,1 +1,22 @@
+import csv
+from pathlib import Path
 
+
+def load_companies(csv_path="config/companies.csv"):
+
+    companies = []
+
+    csv_file = Path(csv_path)
+
+    if not csv_file.exists():
+        raise FileNotFoundError(f"{csv_path} not found.")
+
+    with open(csv_file, newline="", encoding="utf-8-sig") as file:
+
+        reader = csv.DictReader(file)
+
+        for row in reader:
+
+            companies.append(row)
+
+    return companies
