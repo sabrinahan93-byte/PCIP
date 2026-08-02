8251d0be786d85d6e28f1b5cf0e9d2ff15865f4a
sabrinahan93-byte <sabrinahan93@gmail.com>
2026-08-02 20:51:06 +0800

    Update time.py

--- pcip/utils/time.py
+++ pcip/utils/time.py
@@ -1,1 +1,22 @@
+from datetime import datetime, timezone, timedelta
 
+
+SGT = timezone(
+    timedelta(hours=8)
+)
+
+
+def now_sgt():
+
+    return datetime.now(
+        SGT
+    )
+
+
+def now_sgt_str():
+
+    return datetime.now(
+        SGT
+    ).strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
